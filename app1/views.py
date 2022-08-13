38d28916210a2693909a7a358c1be9ec7a9a4be2
piyush-841313 <piyushkumar.ece23@gmail.com>
2022-08-13 23:19:36 +0530

    db connection and operation

--- app1/views.py
+++ app1/views.py
@@ -1,11 +1,15 @@
-from django.http import HttpResponse
 from django.shortcuts import render
-
+from django.db import connection
 # Create your views here.
 
 def xyz(request):
    return render(request,"index.html")
-
-
 def signup(request):
-   return render(request,"index.html")
+   email=request.GET['email']
+   psw=request.GET['pswname']
+   cursor = connection.cursor()
+   query = "insert into users(email,password) values (%s,%s)"
+   value = (email,psw)
+   cursor.execute(query,value)
+   data = {"email":email, "password":psw}
+   return render(request,"first.html",data)
