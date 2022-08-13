a17fa573528adec216cb62c4195c0f507e7b02dc
piyush-841313 <piyushkumar.ece23@gmail.com>
2022-08-13 18:36:20 +0530

    first commit

---
+++ app1/views.py
@@ -0,0 +1,11 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+
+def xyz(request):
+   return render(request,"index.html")
+
+
+def signup(request):
+   return render(request,"index.html")
